d15cca02d69d01f4f78050095444ebfbf8aaccaf
aksio <aksiorex@gmail.com>
2023-06-09 18:49:32 +0200

    Added the background and made it move

--- src/window.py
+++ src/window.py
@@ -1,6 +1,9 @@
 DEFAULT_PLAYER_SIZE = (50, 50)
-DEFAULT_PLAYER_POSITION = (300, 300)
-DEFAULT_PLAYER_SPEED = 5
+DEFAULT_PLAYER_POSITION = (0, 0)
+DEFAULT_PLAYER_SPEED = 2
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
 
 import pygame
 from typing import Tuple
@@ -14,8 +17,23 @@ def add_player(player, window) -> None:
     """
 
     players_spaceship = pygame.transform.scale(player.image, player.size)
-    window.blit(players_spaceship, player.position)
 
+    # Calculating center of the screen for displaying player model
+    x_player = WINDOW_WIDTH / 2 - DEFAULT_PLAYER_SIZE[0] / 2
+    y_player = WINDOW_HEIGHT / 2 - DEFAULT_PLAYER_SIZE[1] / 2
+    
+    window.blit(players_spaceship, (x_player, y_player))
+
+def fill_surrounding_chunks(window, background, offset):
+    off_x, off_y = offset
+
+    window.fill((0, 0, 0))
+
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            x = dx * WINDOW_WIDTH + off_x
+            y = dy * WINDOW_HEIGHT + off_y
+            window.blit(background, (x, y))
 
 def check_if_the_player_is_moving(player, event) -> None:
     """
@@ -56,12 +74,21 @@ def create_window() -> None:
     """
 
     pygame.init()
-    window = pygame.display.set_mode((800, 600))
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
     pygame.display.set_caption("Space survivor")
     #set_icon("spaceship.png")
     
     player = Player(DEFAULT_PLAYER_SIZE, DEFAULT_PLAYER_POSITION, DEFAULT_PLAYER_SPEED, "assets/spaceship.png")
+
+
+    # Loading the background image
+    background_image = pygame.image.load("assets/background.png")
+    background_image = pygame.transform.scale(background_image,(WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    # Create variables to store the background position
+    bg_x = 0
+    bg_y = 0
 
     # Game loop
     running = True
@@ -71,6 +98,18 @@ def create_window() -> None:
                 running = False
             check_if_the_player_is_moving(player, event)
         
-        window.fill((0,0,0)) # Sets the window colour according to RGB
+        bg_x -= player.movement[2] * player.speed  # Move left
+        bg_x += player.movement[0] * player.speed  # Move right
+        bg_y += player.movement[1] * player.speed  # Move up
+        bg_y -= player.movement[3] * player.speed  # Move down
+
+        fill_surrounding_chunks(window, background_image, (bg_x, bg_y))
+
+        if abs(bg_x) >= WINDOW_WIDTH:
+            bg_x = 0
+
+        if abs(bg_y) >= WINDOW_HEIGHT:
+            bg_y = 0
+
         add_player(player, window)
         pygame.display.update() 
